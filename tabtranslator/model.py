7d63b3545a822fd619dac5ba22f7744f7066d19e
Victor <vic.toad.tor@free.fr>
2015-07-29 02:04:33 +0200

    CLEAN renamed classes with CamelCase

--- tabtranslator/model.py
+++ tabtranslator/model.py
@@ -1,41 +1,39 @@
-
-class note(object):
+class Note(object):
 	"""docstring for note"""
 	def __init__(self, name, start=0, lenght=1):
-		super(note, self).__init__()
+		super(Note, self).__init__()
 		self.name = name
 		self.start = start
 		self.lenght = lenght
 
-
-class bar(object):
+class Bar(object):
 	"""docstring for bar"""
 	def __init__(self, lenght = 4):
-		super(bar, self).__init__()
+		super(Bar, self).__init__()
 		self.lenght = lenght
 		self.notes = list()
 
 	def add_note(note):
 		self.notes.append(note)
-		
-class stave(object):
+
+class Stave(object):
 	"""docstring for stave"""
 	def __init__(self, name):
-		super(stave, self).__init__()
+		super(Stave, self).__init__()
 		self.name = name
 		self.bars = list()
 
 	def add_bar(bar):
 		self.bars.append(bar)
-		
-class tab(object):
+
+class Tab(object):
 	"""docstring for tab"""
 	def __init__(self, name):
-		super(tab, self).__init__()
+		super(Tab, self).__init__()
 		self.name
 		self.bars = list()
 
 	def add_bar(bar):
 		self.bars.append(bar)
 
-		
+
