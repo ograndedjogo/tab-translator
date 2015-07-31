7a612cafbc3c61cb60e28427a763a86df87834bf
Lamaw <amahouin@free.fr>
2015-08-01 01:01:21 +0200

    Data model improved : => tab and stave classes now extend from sheet. Existing methods and attributes going to sheet // bar.notes transformed from list() to dict() to map note and start_time

--- tabtranslator/model.py
+++ tabtranslator/model.py
@@ -1,38 +1,38 @@
+class sheet(object):
+	"""docstring for sheet"""
+	def __init__(self, name):
+		super(sheet, self).__init__()
+		self.name = name
+		self.bars = list()
+		
+	def add_bar(bar):
+		self.bars.append(bar)
+
+class Stave(sheet):
+	"""docstring for stave"""
+	def __init__(self):
+		super(Stave, self).__init__()
+
+
+class Tab(sheet):
+	"""docstring for tab"""
+	def __init__(self):
+		super(Tab, self).__init__()
+
+
+class Bar(object):
+	"""docstring for bar"""
+	def __init__(self, cycle = 4):
+		super(Bar, self).__init__()
+		self.cycle = cycle
+		self.notes = dict()
+
+	def add_note(note, start_time):
+		self.notes[note] = start_time
+
 class Note(object):
 	"""docstring for note"""
 	def __init__(self, pitch, duration=1):
 		super(Note, self).__init__()
 		self.pitch = pitch
 		self.duration = duration
-
-class Bar(object):
-	"""docstring for bar"""
-	def __init__(self, lenght = 4):
-		super(Bar, self).__init__()
-		self.lenght = lenght
-		self.notes = list()
-
-	def add_note(note):
-		self.notes.append(note)
-
-class Stave(object):
-	"""docstring for stave"""
-	def __init__(self, name):
-		super(Stave, self).__init__()
-		self.name = name
-		self.bars = list()
-
-	def add_bar(bar):
-		self.bars.append(bar)
-
-class Tab(object):
-	"""docstring for tab"""
-	def __init__(self, name):
-		super(Tab, self).__init__()
-		self.name
-		self.bars = list()
-
-	def add_bar(bar):
-		self.bars.append(bar)
-
-
