2bddf5c9887f35298a67a30966ee4218b1d45efd
Victor <vic.toad.tor@free.fr>
2015-11-22 19:31:37 +0100

    CLEAN

--- notify_tests_passed.sh
+++ notify_tests_passed.sh
@@ -1,13 +1,13 @@
+set -x
 MESSAGE="Tabt+build+no+$TRAVIS_BUILD_NUMBER"
 if [ -f 'testresults' ]; then
     TEST_MESSAGE=`cat testresults | tail -n 1`
     TEST_MESSAGE=${TEST_MESSAGE//=/}
     TEST_MESSAGE=${TEST_MESSAGE// /+}
     MESSAGE="$MESSAGE%3A$TEST_MESSAGE"
-    echo $URL
 else
     MESSAGE="$MESSAGE+finished+with+no+test+results"
 fi
 
 URL="https://smsapi.free-mobile.fr/sendmsg?user=10194111&pass=GC2q9I0YpfpbfK&msg=$MESSAGE"
-wget -qO- $URL &> /dev/null
+wget -qO- $URL
