fa36ee4ae39f1205366b9bae17e028d3c62f1b15
Victor <vic.toad.tor@free.fr>
2015-11-22 19:03:24 +0100

    IMPROVE notif

--- notify_tests_passed.sh
+++ notify_tests_passed.sh
@@ -1,8 +1,13 @@
+MESSAGE="Tabt+build+no+$TRAVIS_BUILD_NUMBER"
 if [ -f 'testresults' ]; then
-    MESSAGE=`cat testresults | tail -n 1`
-    MESSAGE=${MESSAGE//=/}
-    MESSAGE=${MESSAGE// /+}
-    MESSAGE="Tabt+build+no+$TRAVIS_BUILD_NUMBER%3A$MESSAGE"
-    URL="https://smsapi.free-mobile.fr/sendmsg?user=10194111&pass=GC2q9I0YpfpbfK&msg=$MESSAGE"
-    wget -qO- $URL &> /dev/null
+    TEST_MESSAGE=`cat testresults | tail -n 1`
+    TEST_MESSAGE=${TEST_MESSAGE//=/}
+    TEST_MESSAGE=${TEST_MESSAGE// /+}
+    MESSAGE="$MESSAGE%3A$TEST_MESSAGE"
+    echo $URL
+else
+    MESSAGE="$MESSAGE+finished+with+no+test+results"
 fi
+
+URL="https://smsapi.free-mobile.fr/sendmsg?user=10194111&pass=GC2q9I0YpfpbfK&msg=$MESSAGE"
+wget -qO- $URL &> /dev/null
